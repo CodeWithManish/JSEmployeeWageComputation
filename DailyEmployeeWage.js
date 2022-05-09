ef1cffbd66b86528f05d4734fce0fbd482dfafe6
Manish <manishkumar0156@gmail.com>
2022-05-09 15:05:23 +0530

    [Manish] Add: Calculating daily wages of Employee

---
+++ DailyEmployeeWage.js
@@ -0,0 +1,26 @@
+//UC2
+
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const Full_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20
+
+let empHRS = 0;
+empCheck = Math.floor(Math.random() * 10) %3;
+
+switch (empCheck)
+{
+    case IS_PART_TIME:
+        empHRS = PART_TIME_HOURS;
+        break;
+
+    case IS_FULL_TIME:
+        empHRS = Full_TIME_HOURS;
+        break;
+
+    default:
+        empHRS = 0;
+}
+let empWage = empHRS * WAGE_PER_HOUR;
+console.log("Employee Wage : " + empWage);
